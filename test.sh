174cde6ba206706fbdcff11096db2a2395bd6133
Benedikt Franke <benedikt.franke@mll.com>
2024-07-09 12:22:02 +0200

    Update to Rocket 0.5

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -eux
 
-curl -X POST --data-urlencode "markdown=$(cat README.md)" --data-urlencode "css=h1 { color: blue; }" --output README.pdf localhost:8000
+curl --request POST \
+  --data-urlencode "markdown=$(cat README.md)" \
+  --data-urlencode "css=h1 { color: blue; }" \
+  --output README.pdf \
+  localhost:8000
